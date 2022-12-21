5c3fe9ae627ce86a761f612ab963bbb43ce86a4b
liever.rojas <liever.rojas@bitsamericas.com>
2022-12-21 09:38:15 -0500

    Instalación de Express.js y tu primer servidor HTTP

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('My app');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (require, response) => {
+    response.send('Hola mi server en express');
+})
+
+app.listen(port, () => {
+    console.log('my port ' + port)
+});
