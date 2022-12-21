33cab8521365d08e6068db1dbce33d408f0a6d76
liever.rojas <liever.rojas@bitsamericas.com>
2022-12-21 10:54:15 -0500

    GET: parámetros query

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const faker = require('faker');
+
 const app = express();
 const port = 3000;
 
@@ -14,16 +16,22 @@ app.get('/nueva-ruta', (require, response) => {
 })
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000
-    },
-    {
-      name: 'Product 2',
-      price: 3000
-    }
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    })
+  }
+  res.json(products);
+})
+
+app.get('/products/filter', (req, res) => {
+  res.send('Yo soy un filter');
 })
 
 app.get('/products/:id', (req, res) => {
@@ -34,6 +42,18 @@ app.get('/products/:id', (req, res) => {
     name: 'Product 2',
     price: 3000
   });
+})
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    });
+  } else {
+    res.send('No hay parametros');
+  }
 })
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
