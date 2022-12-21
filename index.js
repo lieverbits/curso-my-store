94684fefe36e016a596a9c3ee0df2044dfd9c3a8
liever.rojas <liever.rojas@bitsamericas.com>
2022-12-21 10:28:17 -0500

    GET: recibir parámetros

--- index.js
+++ index.js
@@ -13,6 +13,37 @@ app.get('/nueva-ruta', (require, response) => {
   });
 })
 
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000
+    },
+    {
+      name: 'Product 2',
+      price: 3000
+    }
+  ]);
+})
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 3000
+  });
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId
+  });
+})
+
 app.listen(port, () => {
     console.log('my port ' + port)
 });
