57bb82d367f8d85999edccfc4668950c97e3bdda
liever.rojas <liever.rojas@bitsamericas.com>
2022-12-21 15:18:38 -0500

    ntroducción a servicios: crea tu primer servicio

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,35 +1,22 @@
 const express=require('express');
-const faker=require('faker');
-const router=express.Router();
+const ProductsService = require('./../services/product.service')
+
+const router = express.Router();
+const service = new ProductsService();
+
 router.get('/',(req,res)=>{
-  const products=[];
-  const{size}=req.query;
-  const limit=size||10;
-  for(let index=0;index<limit;index++){
-    products.push({
-      name:faker.commerce.productName(),
-      price:parseInt(faker.commerce.price(),10),
-      image:faker.image.imageUrl(),
-    });
-  }
-  res.json(products);});
+  const products = service.find();
+  res.json(products);
+});
+
 router.get('/filter',(req,res)=>{
   res.send('Yo soy un filter');
 });
 
 router.get('/:id',(req,res)=>{
   const{ id } = req.params;
-  if (id === '999') {
-    res.status(404).json({
-      message: 'not found'
-    })
-  } else {
-    res.status(200).json({
-      id,
-      name: "Product 2",
-      price: 1500
-    });
-  }
+  const product = service.findOne(id);
+  res.json(product);
 });
 
 router.post('/', (req, res) => {
