c2132034418f21d6699262c08c71d56fb975e0bb
liever.rojas <liever.rojas@bitsamericas.com>
2022-12-21 12:11:31 -0500

    PUT, PATCH y DELETE

--- routes/products.router.js
+++ routes/products.router.js
@@ -56,4 +56,23 @@ router.post('/', (req, res) => {
     data: body
   })
 })
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
+  });
+})
+
 module.exports=router;
