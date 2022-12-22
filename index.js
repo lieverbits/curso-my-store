4c7106504ece6df86abfba20f06db758dc9aea98
liever.rojas <liever.rojas@bitsamericas.com>
2022-12-22 15:39:16 -0500

    Ajuste para cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const whiteList = ['http://localhost:8080', 'https://myapp.co']
 const options = {
   origin: (origin, callback) => {
-    if (whiteList.includes(origin)) {
+    if (whiteList.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('No permitido'));
